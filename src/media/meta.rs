4576cc13978d525843d4af8be2312504c4f24c4e
Bulat Sh. (fuderis) <synapdrake@ya.ru>
2026-07-04 06:52:27 +0500

    add MediaMetadata display implementation & update feature 'full'

--- src/media/meta.rs
+++ src/media/meta.rs
@@ -14,3 +14,26 @@ pub struct MediaMetadata {
 
     pub artwork_url: Option<String>,
 }
+
+impl std::fmt::Display for MediaMetadata {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "=== MEDIA METADATA ===")?;
+        writeln!(f, "Title         : {}", self.title)?;
+        writeln!(f, "Artist        : {}", self.artist)?;
+        writeln!(f, "Album         : {}", self.album)?;
+        writeln!(f, "Duration      : {:.2?}", self.duration)?;
+        writeln!(f, "Position      : {:.2?}", self.position)?;
+        writeln!(
+            f,
+            "Playing       : {}",
+            if self.playing { "Yes" } else { "No" }
+        )?;
+        writeln!(
+            f,
+            "Artwork URL   : {}",
+            self.artwork_url.as_deref().unwrap_or("None")
+        )?;
+
+        Ok(())
+    }
+}
